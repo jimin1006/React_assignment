cf1a2db39f7da99b6e386379e4637689db6793fd
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-06-29 13:25:15 +0900

    Your First Component-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-//Chanllenge 1 of 4
-//export default 추가해줘야 함
+//Chanllenge 2 of 4
+//return문을 한줄로
 
 export default function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/lICfvbD.jpg"
-      alt="Aklilu Lemma"
-    />
-  );
+  return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
 }
