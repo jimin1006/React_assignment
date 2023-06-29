eecaad666a480cef7218172818e95e07740a41f1
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-06-29 13:45:30 +0900

    Your First Component-4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
-//Chanllenge 3 of 4
-//소문자면 html 요소로, 대문자로 시작하면 컴포넌트로 인식
-
-function Profile() {
+//Chanllenge 4 of 4
+export default function Congratulations() {
   return (
-    <img
-      src="https://i.imgur.com/QIrZWGIs.jpg"
-      alt="Alan L. Hart"
-    />
-  );
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <h1>Good job!</h1>
   );
 }
