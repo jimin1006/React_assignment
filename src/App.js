219bd260a936ea50ed61af6dae2063c37966c213
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-06-29 13:20:08 +0900

    Your First Component-1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//Chanllenge 1 of 4
+//export default 추가해줘야 함
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/lICfvbD.jpg"
+      alt="Aklilu Lemma"
+    />
+  );
+}
