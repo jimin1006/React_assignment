bfe6d5ee0afd4925ce29c009b5dacc0b429d71ff
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-06-29 14:23:04 +0900

    Rendering Lists-2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-//Challenge 1 of 4 
+//Challenge 2 of 4 
 
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+import { recipes } from './data.js';
 
-export default function List() {
-  const chemists = people.filter(person =>
-    person.profession === 'chemist'
-  );
-  const everyoneElse = people.filter(person =>
-    person.profession !== 'chemist'
-  );
+export default function RecipeList() {
   return (
-    <article>
-      <h1>Scientists</h1>
-      <h2>Chemists</h2>
-      <ul>
-        {chemists.map(person =>
-          <li key={person.id}>
-            <img
-              src={getImageUrl(person)}
-              alt={person.name}
-            />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        )}
-      </ul>
-      <h2>Everyone Else</h2>
-      <ul>
-        {everyoneElse.map(person =>
-          <li key={person.id}>
-            <img
-              src={getImageUrl(person)}
-              alt={person.name}
-            />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        )}
-      </ul>
-    </article>
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
-
