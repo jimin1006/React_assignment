b485627baaaab4dcc33e57976f6d760b39542025
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-07-03 02:56:35 +0900

    Fix a request counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-//Challenge 1 of 1: Implement a traffic light
+//Challenge 1 of 2: Fix a request counter
 
 import { useState } from 'react';
 
-export default function TrafficLight() {
-  const [walk, setWalk] = useState(true);
-
-  function handleClick() {
-    setWalk(!walk);
-    alert(walk ? 'Stop is next' : 'Walk is next')
+export default function RequestTracker() {
+  //pending = 0
+  const [pending, setPending] = useState(0);
+  //completed = 0
+  const [completed, setCompleted] = useState(0);
+  //이벤트 핸들러
+  async function handleClick() {
+    setPending(pending + 1); 
+    await delay(3000);
+    setPending(n => n - 1); 
+    setCompleted(n => n + 1);
   }
 
   return (
     <>
+      <h3>
+        Pending: {pending}
+      </h3>
+      <h3>
+        Completed: {completed}
+      </h3>
       <button onClick={handleClick}>
-        Change to {walk ? 'Stop' : 'Walk'}
+        Buy     
       </button>
-      <h1 style={{
-        color: walk ? 'darkgreen' : 'darkred'
-      }}>
-        {walk ? 'Walk' : 'Stop'}
-      </h1>
     </>
   );
 }
