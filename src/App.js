e5c966bfdc2000ead2f3b5dc9fa0769f1b81a4c0
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-07-03 01:31:23 +0900

    Implement a traffic light

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-//Challenge 4 of 4: Remove unnecessary state
-//state를 삭제하라는 힌트 없었으면 해결 못했을듯
-//렌더링이 한 번밖에 일어나지 않아서 state 대신 일반변수로 고정하는듯
+//Challenge 1 of 1: Implement a traffic light
+
 import { useState } from 'react';
 
-export default function FeedbackForm() {
-  let name= '';
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
 
   function handleClick() {
-    name = prompt('What is your name?');
-    alert(`Hello, ${name}!`);
+    setWalk(!walk);
+    alert(walk ? 'Stop is next' : 'Walk is next')
   }
 
   return (
-    <button onClick={handleClick}>
-      Greet
-    </button>
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
   );
 }
